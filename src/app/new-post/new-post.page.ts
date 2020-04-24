da7f400c79e11f41f10c40c45eba9744b00f75ba
Miguel Zambrano <miguelz@email.sc.edu>
2020-04-24 14:38:40 -0400

    camera

--- src/app/new-post/new-post.page.ts
+++ src/app/new-post/new-post.page.ts
@@ -7,7 +7,8 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { firestore } from 'firebase/app';
 import * as firebase from "firebase";
 import { AuthService } from '../auth.service';
-
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { File } from "@ionic-native/file/ngx";
 
 @Component({
   selector: 'app-new-post',
@@ -15,27 +16,32 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./new-post.page.scss'],
 })
 export class NewPostPage implements OnInit {
+  imgfile="";
   new_item_form: FormGroup;
   constructor(private router: Router,
- 	 public formBuilder: FormBuilder,
+    public formBuilder: FormBuilder,
+    private camera:Camera,
+    private file:File,
         public itemService: ItemService,
         public afstore: AngularFirestore,public authService: AuthService) { }
 
   ngOnInit() {
+    this.captureImage();
     this.new_item_form = this.formBuilder.group({
       title: new FormControl('', Validators.required),
     });
   }
 
   createItem(value){
-  	console.log(value.BS);
+  //console.log(value.BS);
   	
     let randomId = Math.random().toString(36).substr(2, 20);
     let data = {
     id: randomId,
     title: value.title,
     vibe: this.vibeCheck(value.title),
-    username: this.authService.getUser()
+    username: this.authService.getUser(),
+    img: this.imgfile
     }
     
     
@@ -128,6 +134,108 @@ if(title.includes('plur')){
 }
     return boast
   }
+
+
+  async captureImage() {
+    const options: CameraOptions = {
+      quality: 30,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    try{
+      console.log(this);
+      let cameraInfo = await this.camera.getPicture(options);
+      let blobInfo = await this.makeFileIntoBlob(cameraInfo);
+      let uploadInfo: any = await this.uploadToFirebase(blobInfo);
+      console.log(uploadInfo);
+      // let url:any = uploadInfo.ref.getDownloadURL();
+      alert("File Upload Success " + uploadInfo);
+      this.imgfile = uploadInfo;
+      
+    } catch (e) {
+      console.log(e.message);
+      alert("File Upload Error " + e.message);
+    }
+  }
+  makeFileIntoBlob(_imagePath) {
+    // INSTALL PLUGIN - cordova plugin add cordova-plugin-file
+    return new Promise((resolve, reject) => {
+      let fileName = "";
+      this.file
+        .resolveLocalFilesystemUrl(_imagePath)
+        .then(fileEntry => {
+          let { name, nativeURL } = fileEntry;
+
+          // get the path..
+          let path = nativeURL.substring(0, nativeURL.lastIndexOf("/"));
+          console.log("path", path);
+          console.log("fileName", name);
+
+          fileName = name;
+
+          // we are provided the name, so now read the file into
+          // a buffer
+          return this.file.readAsArrayBuffer(path, name);
+        })
+        .then(buffer => {
+          // get the buffer and make a blob to be saved
+          let imgBlob = new Blob([buffer], {
+            type: "image/jpeg"
+          });
+          console.log(imgBlob.type, imgBlob.size);
+          resolve({
+            fileName,
+            imgBlob
+          });
+        })
+        .catch(e => reject(e));
+    });
+  }
+
+  /**
+   *
+   * @param _imageBlobInfo
+   */
+  uploadToFirebase(_imageBlobInfo) {
+    console.log("uploadToFirebase");
+    return new Promise((resolve, reject) => {
+      let imageid = (Math.floor(Math.random() * 2000)).toString();
+      let filename = "menu_"+imageid
+      // filename = _imageBlobInfo.fileName;
+      let fileRef = firebase.storage().ref("images/" + filename);
+
+      let uploadTask = fileRef.put(_imageBlobInfo.imgBlob);
+      let mydownloadurl="";
+      
+
+      uploadTask.on(
+        "state_changed",
+        (_snapshot: any) => {
+          console.log(
+            "snapshot progess " +
+              (_snapshot.bytesTransferred / _snapshot.totalBytes) * 100
+          );
+        },
+        _error => {
+          console.log(_error);
+          reject(_error);
+        },
+        () => {
+          // completion...  get the image URL for saving to database
+          uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+            console.log('File available at', downloadURL);
+            mydownloadurl = downloadURL;
+            resolve( mydownloadurl);
+          });
+          // resolve( uploadTask.snapshot);
+          // resolve( mydownloadurl);
+
+        }
+      );
+    });
+  }
+
 
   goBack(){
     this.router.navigate(['/tabs/tab2']);
