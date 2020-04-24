38115a4b6562a476d238b33943f4d50afab54b50
chaseoconnor <chaseoc98@gmail.com>
2020-04-24 16:45:57 -0400

    Working memories

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -25,6 +25,7 @@ export class ItemService {
     .toPromise()
     .then(snapshot => {
       snapshot.forEach(doc => {
+        console.log(doc.data().username)
         this.post.push(doc.data())
       });
     })
@@ -32,21 +33,36 @@ export class ItemService {
   }
 
   getImages(){
+    for (let i=0; i<this.post.length; i++){
+
+      if (this.post[i].username == this.user.getUser()){
+        this.memories.push(this.post[i])
+        console.log("Success")
+      }else{
+        console.log("Failed")
+      }
+    }
+    return this.memories;
+  } 
+  /*getImages(){
     this.postRefresh();
-    let store = this.afstore.collection('post');
-    let Items =  store.get()
+    let af = this.afstore.collection('post');
+    let Items2 =  af.get()
     .toPromise()
     .then(snapshot => {
-    snapshot.forEach(doc => {
-        if (doc.data().username == this.user.getUser()) {
-            this.memories.push(doc.data())
+      snapshot.forEach(doc => {
+        console.log(doc.data().username)
+        //console.log(this.user.getUser(), "getUser")
+        if (doc.data().username.equals(this.user.getUser())) {
+          console.log("Image pulled")
+            this.memories.push(doc.data())   
         }else{
             console.log("Not in user storage");
         }
-  });
-})
-return this.memories;
-}
+      });
+    })
+    return this.memories;
+  }*/
 
   createPost(title){
     let randomId = Math.random().toString(36).substr(2, 5);
